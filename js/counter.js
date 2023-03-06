851cc4faeb98ba526b3cafa27d93d7b0036465f5
Umut Çakmak <umutcakmakcontact@gmail.com>
2023-03-06 16:55:34 +0300

    Reset button and LocalStorage added

--- js/counter.js
+++ js/counter.js
@@ -1,37 +1,49 @@
-let counter= document.querySelector("#counter")
-let decrease= document.querySelector("#decrease")
-let increase= document.querySelector("#increase")
+let counter = document.querySelector("#counter")
+let decrease = document.querySelector("#decrease")
+let increase = document.querySelector("#increase")
+let reset = document.querySelector("#reset")
 //I provided access with IDs.
 
-let number = 0
+let number = localStorage.getItem("counter") ? parseInt(localStorage.getItem("counter")) : 0
 //I set the initial state of the counter to 0.
 
-if(parseInt(counter.innerHTML) >= 0){
+if (parseInt(counter.innerHTML) >= 0) {
     number = parseInt(counter.innerHTML)
 }
 //If the counter starting number from html is positive, I entered the value.
 
-counter.innerHTML=number
+counter.innerHTML = number
 
 const inClick = () => {
-    number+=1
-    counter.innerHTML=number
+    number += 1
+    counter.innerHTML = number
+    localStorage.setItem("counter", number)
     //I defined the feature to increase by 1 when clicking the button.
 }
 
 const deClick = () => {
-    if(number > 0){
-        number-=1
-        counter.innerHTML=number
-    }else if(number == 0){
-        counter.innerHTML=number
-    }else if(number < 0){
-        counter.innerHTML="Counter isn't be small from zero."
+    if (number > 0) {
+        number -= 1
+        counter.innerHTML = number
+        localStorage.setItem("counter", number)
+    } else if (number == 0) {
+        counter.innerHTML = number
+        localStorage.setItem("counter", number)
+    } else if (number < 0) {
+        counter.innerHTML = "Counter isn't be small from zero."
+        localStorage.setItem("counter", number)
     }
     //I defined the feature to decrease by 1 when clicking the button.
     //I added a limit of not being less than 0 with the if-else command.
 }
 
+const resetClick = () => {
+    number = 0
+    counter.innerHTML = number
+    localStorage.setItem("counter", number)
+}
+
 increase.addEventListener("click", inClick)
 decrease.addEventListener("click", deClick)
+reset.addEventListener("click", resetClick)
 //I defined what to do when the button is clicked.
